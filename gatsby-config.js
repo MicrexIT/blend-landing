06ab107c3b5f0af04914978e702eb7a97fc817a4
Michele Rexha <rexha.michele@gmail.com>
2021-03-12 19:57:49 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: "blend-landing",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
